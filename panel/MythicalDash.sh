6e6977a578fee9aa0af7f09f87ea3e07388b6c1a
InfiniteandRaju <opInfinite4@gmail.com>
2025-12-10 18:12:01 +0530

    Add MythicalDash installation script

---
+++ panel/MythicalDash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RED='\033[1;31m'; GREEN='\033[1;32m'; YELLOW='\033[1;33m'; CYAN='\033[1;36m'; PURPLE='\033[1;35m'; WHITE='\033[1;37m'; NC='\033[0m'
+
+clear
+echo -e "${PURPLE}   ╔════════════════════════════════════════════════════════╗${NC}"
+echo -e "${PURPLE}   ║${WHITE}   Made By : ${CYAN}Infinite21 ${WHITE}& ${GREEN}Staxxy Rai   ${PURPLE}║${NC}"
+echo -e "${PURPLE}   ╚════════════════════════════════════════════════════════╝${NC}"
+echo -e "${WHITE}   [ ${CYAN}MYTHICAL DASHBOARD INSTALLER${WHITE} ]${NC}"
+echo -e ""
+read -p "   Press Enter to begin..."
+
+bash <(curl -s https://raw.githubusercontent.com/MythicalLTD/MythicalDash/main/install.sh)
