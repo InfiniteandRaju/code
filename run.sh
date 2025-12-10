ee2de9a6140aab480d84f0512b11ed2ccb46830f
InfiniteandRaju <opInfinite4@gmail.com>
2025-12-10 14:08:07 +0530

    Add script to initialize git repository and push

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# -150" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/InfiniteandRaju/code.git
+git push -u origin main
