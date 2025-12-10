4b0866b8b8dc36977784fd3368219b268107d047
InfiniteandRaju <opInfinite4@gmail.com>
2025-12-10 18:11:00 +0530

    Create Jexactyl.sh

---
+++ panel/Jexactyl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+RED='\033[1;31m'; GREEN='\033[1;32m'; YELLOW='\033[1;33m'; CYAN='\033[1;36m'; PURPLE='\033[1;35m'; WHITE='\033[1;37m'; NC='\033[0m'
+
+clear
+echo -e "${PURPLE}   ╔════════════════════════════════════════════════════════╗${NC}"
+echo -e "${PURPLE}   ║${WHITE}   Made By : ${CYAN}Infinite21 ${WHITE}& ${GREEN}Staxxy Rai   ${PURPLE}║${NC}"
+echo -e "${PURPLE}   ╚════════════════════════════════════════════════════════╝${NC}"
+echo -e "${WHITE}   [ ${PURPLE}JEXACTYL PANEL INSTALLER${WHITE} ]${NC}"
+echo -e ""
+read -p "   Press Enter to begin..."
+
+# Jexactyl Official Script
+bash <(curl -s https://raw.githubusercontent.com/Jexactyl/Jexactyl-Installer/main/install.sh)
